997a057a6e3ae08f6545e3c806744151e7becff9
Joel Vardy <info@joelvardy.com>
2014-08-30 15:47:22 +0100

    Updated demo

---
+++ app.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+
+	'use strict';
+
+	// Initialise resize library
+	var resize = new window.resize();
+	resize.init();
+
+	// Upload photo
+	var upload = function (photo, callback) {
+		var formData = new FormData();
+		formData.append('photo', photo);
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState === 4) {
+				callback(request.response);
+			}
+		}
+		request.open('POST', './process.php');
+		request.responseType = 'json';
+		request.send(formData);
+	};
+
+	var fileSize = function (size) {
+		var i = Math.floor(Math.log(size) / Math.log(1024));
+		return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+	};
+
+	document.querySelector('form input[type=file]').addEventListener('change', function (event) {
+		event.preventDefault();
+
+		var files = event.target.files;
+		for (var i in files) {
+
+			if (typeof files[i] !== 'object') return false;
+
+			(function () {
+
+				var initialSize = files[i].size;
+
+				resize.photo(files[i], 2000, 'file', function (resizedFile) {
+
+					var resizedSize = resizedFile.size;
+
+					upload(resizedFile, function (response) {
+						var rowElement = document.createElement('tr');
+						rowElement.innerHTML = '<td>'+new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds()+'</td><td>'+fileSize(initialSize)+'</td><td>'+fileSize(resizedSize)+'</td><td><a href="'+response.url+'">view image</a></td>';
+						document.querySelector('table.images tbody').appendChild(rowElement);
+					});
+
+					resize.photo(resizedFile, 500, 'dataURL', function (thumbnail) {
+						console.log('Display the thumbnail to the user: ', thumbnail);
+					});
+
+				});
+
+			}());
+
+		}
+
+	});
+
+});
--- readme.md
+++ readme.md
@@ -2,8 +2,10 @@
 
 This is the source code for the [JavaScript Image Upload Demo][demo] which I wrote about on [my website][writing] in July 2013.
 
+I updated the article, and this demo in August 2014.
+
 [Joel Vardy][joelvardy]
 
-  [joelvardy]: https://joelvardy.com/
+  [joelvardy]: https://joelvardy.com
   [writing]: http://joelvardy.com/writing/javascript-image-upload
-  [demo]: http://demo.joelvardy.com/uploads/
+  [demo]: http://demo.joelvardy.com/uploads
