2d6100efdd11672c954d85f7bc91a80cc1cad5e7
Joel Vardy <info@joelvardy.com>
2014-08-30 15:41:38 +0100

    Process photo as a file (rather than dataURL

--- process.php
+++ process.php
@@ -1,23 +1,14 @@
 <?php
 
-// Generate filename
 $filename = md5(mt_rand()).'.jpg';
+$status = (boolean) move_uploaded_file($_FILES['photo']['tmp_name'], './images/'.$filename);
 
-// Read RAW data
-$data = file_get_contents('php://input');
+$response = (object) [
+	'status' => $status
+];
 
-// Read string as an image file
-$image = file_get_contents('data://'.substr($data, 5));
-
-// Save to disk
-if ( ! file_put_contents('images/'.$filename, $image)) {
-	header('HTTP/1.1 503 Service Unavailable');
-	exit();
+if ($status) {
+	$response->url = './images/'.$filename;
 }
 
-// Clean up memory
-unset($data);
-unset($image);
-
-// Return file URL
-echo './images/'.$filename;
+echo json_encode($response);
